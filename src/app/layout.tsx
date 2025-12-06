690b8a89d1b923d34a36db139c127173bcbe7a13
cloudzeus <46929913+cloudzeus@users.noreply.github.com>
2025-12-06 06:36:05 +0200

    Fix Toaster SSR issue with client wrapper component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import dynamic from "next/dynamic";
-
-const Toaster = dynamic(() => import("@/components/ui/sonner").then((mod) => ({ default: mod.Toaster })), {
-  ssr: false,
-});
+import { ToasterWrapper } from "@/components/toaster-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster />
+        <ToasterWrapper />
       </body>
     </html>
   );
